ddcfadfb74a0fcf60c39db133eccf0fef4ed3cfd
Waysidecats <103798521+Waysidecats@users.noreply.github.com>
2025-07-09 15:52:56 -0700

    Update lock.lua

--- lock.lua
+++ lock.lua
@@ -24,13 +24,37 @@ local function readPassword()
   end
 end
 
-term.clear()
-term.setCursorPos(1, 1)
+local function printCentered(text, y)
+  local w, _ = term.getSize()
+  local x = math.floor((w - #text) / 2) + 1
+  term.setCursorPos(x, y)
+  term.write(text)
+end
 
 while true do
+  term.clear()
+  term.setCursorPos(1,1)
+  print("+-----------------------+")
+  print("|    ENTER PASSWORD     |")
+  print("+-----------------------+")
+  term.setCursorPos(1,5)
+  io.write("> ")
   local pw = readPassword()
   if pw == correctPassword then
+    term.clear()
+    term.setCursorPos(1,1)
+    print("+-----------------------+")
+    print("|     ACCESS GRANTED    |")
+    print("+-----------------------+")
+    sleep(1.5)
     shell.run("activate")
     break
+  else
+    term.clear()
+    term.setCursorPos(1,1)
+    print("+-----------------------+")
+    print("|    INCORRECT PASSWORD |")
+    print("+-----------------------+")
+    sleep(1.5)
   end
 end
